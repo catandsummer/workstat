2ce45eb44fb112c70ef03398c935f8c241d2c56c
劳维旻 <laoweimin@corp.netease.com>
2018-07-24 10:36:10 +0800

    初步完成逻辑

--- Database.py
+++ Database.py
@@ -10,8 +10,37 @@ def get_mongo_db(dbname):
     url = "mongodb://%s:%s@%s:%s" % (quote("admin"), quote("admin"), "192.168.1.105", "27017")
     return pymongo.MongoClient(url)[dbname]
 
+def _getcollect():
+    db = get_mongo_db('workstat_test')
+    return db["raw_data"]
+
+
+def signin(username, passkey):
+    db = get_mongo_db('workstat_test')
+    col = db["auth"]
+    data = col.find_one(name=username)
+    if data:
+        if data["pass"] == passkey:
+            return True
+        else:
+            return False
+    else:
+        # signup
+        col.insert({"name": username, "pass":passkey})
+        return True
+
+
+def save_data(username, data):
+    col = _getcollect()
+    col.insert({"name": username, "data": data})
+
+def get_data(username):
+    col = _getcollect()
+    return list(col.find(name=username))
+
 if __name__ == '__main__':
     db = get_mongo_db('aluba_stat')
     rawdata = db["raw_data"]
+
     for doc in rawdata.find():
         print(doc)
--- app.py
+++ app.py
@@ -1,23 +1,37 @@
 # -*- coding:utf-8 -*-
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request, session
 from flask_login import login_required
 from flask_base import app
 from user import auth
-
+from Database import save_data, get_data
 
 # ==================================
 # 下面路由至页面
 # ==================================
+
 
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
 
-@app.route("/adddata", methods=['POST'])
+
+def makelist(datalist):
+    ret = ""
+    for data in datalist:
+        ret+= "<li>%s</li>\n" % data
+    return ret
+
+@app.route("/summitdata", methods=['POST'])
 @login_required
-def adddata():
-    pass
+def summitdata():
+    data = request.form.to_dict()
+    content = data['content']
+    username = session.get('user_id')
+    save_data(username, content)
+    alldata = get_data(username)
+    return render_template('index.html', content=makelist(alldata))
+
 
 def main():
     app.register_blueprint(auth, url_prefix='/auth')
